bba48ac136f538dac5d45bce03dd2ac4a76b2246
Nils Henrik Aaman <softwareuabhnk@gmail.com>
2018-05-03 16:44:18 +0200

    vpp spring MVC and WebFlow - chapter 7 'Validation'

--- src/com/virtualpairprogrammers/control/CreateBookController.java
+++ src/com/virtualpairprogrammers/control/CreateBookController.java
@@ -2,12 +2,14 @@ package com.virtualpairprogrammers.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.virtualpairprogrammers.domain.Book;
 import com.virtualpairprogrammers.services.BookService;
+import com.virtualpairprogrammers.validation.BookValidator;
 
 @Controller
 @RequestMapping("/addNewBook")
@@ -16,19 +18,23 @@ public class CreateBookController {
 	@Autowired
 	private BookService bookService;
 	
+	@Autowired
+	private BookValidator bookValidator;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView show() {
 		return new ModelAndView("/add-new-book.jsp", "book", new Book());
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView processForm(Book newBook) {
+	public ModelAndView processForm(Book newBook, Errors result) {
+
+		bookValidator.validate(newBook, result);
+		if (result.hasErrors())
+		{
+			return new ModelAndView("/add-new-book.jsp", "book", newBook);
+		}
 		bookService.registerNewBook(newBook);
 		return new ModelAndView("/book-added.jsp", "title", newBook.getTitle());
 	}
-	
-	
-	
-	
-
 }
